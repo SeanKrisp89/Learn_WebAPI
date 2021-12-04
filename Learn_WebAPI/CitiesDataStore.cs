e1abf08d4e995d18554c5dbe1f96f62ac9c4d884
Sean Krispin <sean.krispin@gmail.com>
2021-12-03 20:43:12 -0600

    Added new CitiesDataStore class which is just our data in memory, not data persisted. Additionally, added CityDto class, and updated the GetCities() method to use that in memory data store.

---
+++ Learn_WebAPI/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Learn_WebAPI.Models;
+
+namespace Learn_WebAPI
+{
+	public class CitiesDataStore
+	{
+		public static CitiesDataStore Current { get; } = new CitiesDataStore();//static property that makes sure we can keep working on the same instance so long as we don't restart the web server
+		public List<CityDto> Cities { get; set; }
+
+		public CitiesDataStore()//This is our "In-Memory Data Store"
+		{
+			Cities = new List<CityDto>()
+			{
+				new CityDto()
+				{
+					Id = 1,
+					Name = "New York City",
+					Description = "The one with the big park."
+				},
+				new CityDto()
+				{
+					Id = 2,
+					Name = "Antwerp",
+					Description = "The one with the cathedral that was never really finished."
+				},
+				new CityDto()
+				{
+					Id = 3,
+					Name = "Paris",
+					Description = "The one with that big tower."
+				}
+			};
+		}
+	}
+}
--- Learn_WebAPI/Controllers/CitiesController.cs
+++ Learn_WebAPI/Controllers/CitiesController.cs
@@ -3,23 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Learn_WebAPI.Models;
 
 namespace Learn_WebAPI.Controllers
 {
 	[ApiController]
+	[Route("api/cities")]//This defines the route attribute for all methods in this controller. This is why the "(api/cities)" is commented out in the GetCities route attribute.
 	public class CitiesController : ControllerBase //Controllerbase contains basic func controllers need like access to the modelstate, the current user, and common methods used for returning responses.
 												   //We could also use Controller class, but that also supports views, and those are not needed when building APIs
 	{
 		//We want to return JSON as the format of our data representation...
+		[HttpGet/*("api/cities")*/] // <-- this is "Attribute Routing"
 		public JsonResult GetCities()
 		{
-			//JsonResult returns a Jsonified version of whatever we pass into the constructor of a new JsonResult object
-			return new JsonResult(
-				new List<object>()
-				{
-					new{id = 1, Name = "New York City"},
-					new{id = 2, Name = "Antwerp"}
-				});
+			////JsonResult returns a Jsonified version of whatever we pass into the constructor of a new JsonResult object
+			//return new JsonResult(
+			//	new List<object>()
+			//	{
+			//		new{id = 1, Name = "New York City"},
+			//		new{id = 2, Name = "Antwerp"}
+			//	});
+
+			return new JsonResult(CitiesDataStore.Current.Cities);
 		}
+
+		[HttpGet("{id}")] //Since again, we have the controller route ("api/cities") mapped up top at the controller level, we actually only need the {id} portion of the route attribute 
+		public JsonResult GetCity(int id)
+		{
+			return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+		}
+
+
 	}
 }
