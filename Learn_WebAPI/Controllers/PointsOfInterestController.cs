41f3501a6978215a0ebf49c4f7a3f91e8bb0aa16
Sean Krispin <sean.krispin@gmail.com>
2021-12-04 20:10:16 -0600

    Learned how to create resources with POST methods, as well as where the input format is specified in a request (content-type header) and what impacts that has on deserializing the input.

--- Learn_WebAPI/Controllers/PointsOfInterestController.cs
+++ Learn_WebAPI/Controllers/PointsOfInterestController.cs
@@ -26,7 +26,8 @@ namespace Learn_WebAPI.Controllers
 			return Ok(city.PointsOfInterest);
 		}
 
-		[HttpGet("{id}")]//<-- so here we're only passing in point of interest Id in the HTTPGET attribute because the cityId will always be passed in since it was added in our Route template we added on the controller.
+		[HttpGet("{id}", Name = "GetPointOfInterest")]//<-- so here we're only passing in point of interest Id in the HTTPGET attribute because the cityId will always be passed in since it was added in our Route template we added on the controller.
+		//Additionally, the "Name" property is so that we can refer to it in helper methods like return CreatedAtRoute() - The reason it's on the get is because that's where they'd go to request this resource
 		public IActionResult GetPointOfInterest(int cityId, int id)
 		{
 			//Check for city first
@@ -47,6 +48,46 @@ namespace Learn_WebAPI.Controllers
 			}
 
 			return Ok(pointOfInterest);
+		}
+
+		[HttpPost]
+		public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+		//We want to make sure that this pointOfInterest is de-serialized from the request body; the API Controller attribute takes care of that. The PointOfInterestForCreationDto is considered a complex type and is assumed to be contained in the request body.
+		//So to ensure that, we can decorate this method with the FromBody attribute above.
+		//It is possible that the consumer of the API sends a bad request in such that the request body cannot be de-serialized into a POIFCDTO in which case our POIFCDTO will be null. Thus check for that!
+		{
+			//if(pointOfInterest == null) //So here, the consumer of our API f'ed up and sent over null for POIFCDTO. So we say "bad request buddy"
+			//{
+			//	return BadRequest();
+			//}
+			//HOWEVER - WE DON'T NEED TO WRITE THAT VALIDATION CHECK BECAUSE THE API CONTROLLER ATTRIBUTE AUTOMATICALLY CHECKS FOR THIS
+
+			var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+			if(city == null)
+			{
+				return NotFound();
+			}
+
+			//For demo only - shitty code to grab latest/new Id. 
+			//So first grab the max existing PointOfInterest Id
+			var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+			//Create new POI instance to store data from request body
+			var finalPointOfInterest = new PointOfInterestDto()
+			{
+				Id = ++maxPointOfInterestId, //Increment the Id by 1
+				Name = pointOfInterest.Name, //Set the name equal to that provided by the request body
+				Description = pointOfInterest.Description //Set the description equal to that provided by the request body
+			};
+
+			city.PointsOfInterest.Add(finalPointOfInterest);
+
+			//For POSTs, the recommended response from the server is a 201 Created response (as opposed to a 200 OK, this is more specific saying that the resource was actually created). SO this helper method returns just that
+			return CreatedAtRoute("GetPointOfInterest", 
+				new { cityId = cityId, id = finalPointOfInterest.Id },
+				finalPointOfInterest);	//This allows us to return a response with a location header. That location header will contain the URI where the newly created POI (or resource) can be found. We still have to pass in any arguments required for the controller. So our cityId and PointOfInterestId passed into the method.
+										//Technically we could just pass "cityId" by itself since our method parameter is also called cityId...
 		}
 	}
 }
---
+++ Learn_WebAPI/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_WebAPI.Models
+{
+	public class PointOfInterestForCreationDto //We created this separate POI Data Transfer Object because our first one contains ID. The consumer of our API is not responsible for determining the Id, that's the data-persistence layer's responsibility (the Db).
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+	}
+}
